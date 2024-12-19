1c63b602ad9c7707942ecf610cf88ec94194f46d
dhaneshdhakrey <dhaneshdhakrey@gmail.com>
2024-12-19 11:00:38 +0530

    Environment Setup Push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MainCont from "./Components/MainDIv/MainCont";
+function App() {
+  return (
+    <div>
+      
+    hello
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/addcart-context.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddCart=React.createContext({
+    TotalPrice:0,
+    
+});
+export default AddCart;
