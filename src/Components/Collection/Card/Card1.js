adf762d3dde33f40e095bdb66352a467aee58fbb
Shivam Kataria <133666415+shivamkataria7622@users.noreply.github.com>
2024-12-21 15:14:45 +0530

    basic

--- src/Components/Collection/Card/Card1.js
+++ src/Components/Collection/Card/Card1.js
@@ -4,6 +4,10 @@ import './Card.css'; // Assuming you save the CSS file as Card1.css
 
 function Card1(props) {
   const navigate = useNavigate();
+  let renderColor='green';
+  if(props.price>500){
+    renderColor='red';
+  }
   function handleClick() {
     //console.log(props.price);
     //props.changeCart({price:props.price,id:props.id,imgsrc:props.imgsrc});
@@ -20,7 +24,7 @@ function Card1(props) {
        onClick={handleClick}/>
       
       <div className="card-content">
-        <h2 className="product-title">{props.title}</h2>
+        <h2 className="product-title" style={{color:renderColor }}>{props.title}</h2>
         <p className="product-price">Rs {props.price}</p>
         <p>{props.id}</p>
       </div>
